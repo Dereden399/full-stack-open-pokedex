172348972c60df59876a287b5e562d5ff1301465
denis <denis.kuznetsov@aalto.fi>
2023-06-01 11:13:11 +0300

    11.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,52 @@
 module.exports = {
   "env": {
-    "browser": true,
+    "node": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
   },
   "extends": [
     "eslint:recommended",
-    "plugin:react/recommended"
+    "plugin:react/recommended",
   ],
   "parserOptions": {
     "ecmaFeatures": {
-      "jsx": true
+      "jsx": true,
     },
     "ecmaVersion": 2018,
-    "sourceType": "module"
+    "sourceType": "module",
   },
   "plugins": [
-    "react", "jest"
+    "react",
+    "jest",
   ],
   "rules": {
     "indent": [
       "error",
-        2
-      ],
+      2,
+    ],
     "linebreak-style": [
       "error",
-      "unix"
+      "unix",
     ],
     "quotes": [
       "error",
-      "single"
+      "single",
     ],
     "semi": [
       "error",
-      "never"
+      "never",
     ],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": [
-      "error", "always"
+      "error",
+      "always",
     ],
     "arrow-spacing": [
-      "error", { "before": true, "after": true }
+      "error",
+      { "before": true, "after": true },
     ],
     "no-console": "error",
-    "react/prop-types": 0
-  }
-}
+    "react/prop-types": 0,
+  },
+};
